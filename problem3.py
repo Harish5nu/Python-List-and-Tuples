0fd30869a255b14c0df7df62edd97d60a64c3f0f
Harish Saud <saudharish770@gmail.com>
2025-06-10 22:58:10 +0545

    Create problem3.py

---
+++ problem3.py
@@ -0,0 +1,22 @@
+# Creating a list of numbers
+numbers = [10, 5, 8, 3, 15]
+
+# Printing the list
+print("Original list:", numbers)
+
+# Adding a number
+numbers.append(20)
+print("After adding 20:", numbers)
+
+# Sorting the list
+numbers.sort()
+print("Sorted list:", numbers)
+
+# Finding the maximum number
+print("Maximum number:", max(numbers))
+
+# Finding the minimum number
+print("Minimum number:", min(numbers))
+
+# Calculating the sum of all numbers
+print("Sum of all numbers:", sum(numbers))
