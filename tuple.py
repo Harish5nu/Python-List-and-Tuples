4555033d36f50f5142c0c3c9b3c6e9295a2795a2
Harish Saud <saudharish770@gmail.com>
2025-06-20 22:38:14 +0545

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,15 @@
+# Create tuple
+data = (4, 7, 1, 7, 3, 7)
+
+# Count and index
+print("Count of 7:", data.count(7))
+print("Index of 1:", data.index(1))
+
+# Built-in functions
+print("Length:", len(data))
+print("Max:", max(data))
+print("Min:", min(data))
+print("Sum:", sum(data))
+
+# Slicing
+print("Sliced Tuple:", data[1:4])
